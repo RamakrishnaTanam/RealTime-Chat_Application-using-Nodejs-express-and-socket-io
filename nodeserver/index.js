8830e24458a4178c8220b28939235e89305709f5
Ramakrishna T <147640669+RamakrishnaTanam@users.noreply.github.com>
2025-01-02 23:41:08 +0530

    Update index.js

--- nodeserver/index.js
+++ nodeserver/index.js
@@ -3,24 +3,20 @@ const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 
-// Serve static files (CSS, JS, etc.)
+// Middleware to serve static files (CSS, JS, etc.)
 app.use(express.static('public'));
 
 // Define the root route to serve your HTML file
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html'); // Adjust the path as necessary
+    res.sendFile(`${__dirname}/index.html`); // Adjust the path as necessary
 });
 
-// Handle socket.io connection
+// Handle socket.io connections
 io.on('connection', socket => {
-    console.log('a user connected');
+    console.log('A user connected');
+    
     socket.on('disconnect', () => {
-        console.log('user disconnected');
+        console.log('User disconnected');
     });
 });
 
-// Start the server
-const PORT = 8000;
-http.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-});
